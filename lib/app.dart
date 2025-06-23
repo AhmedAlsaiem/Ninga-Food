8c553082eedf1c3f2f59fc1d01f99c9f6008a35f
AhmedAlsaiem <ahmedalsaiem2025@gmail.com>
2025-06-23 16:40:46 +0300

    create onboarding route and add this route as intial route in material app

--- lib/app.dart
+++ lib/app.dart
@@ -4,7 +4,6 @@ import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:ninga/core/functions/custom_localization_delgate.dart';
 import 'package:ninga/config/theme/dark_theme.dart';
 import 'package:ninga/config/theme/light_theme.dart';
-import 'package:ninga/core/utils/styles_manager.dart';
 
 import 'config/routes/app_router.dart';
 import 'generated/l10n.dart';
@@ -20,56 +19,19 @@ class Ninga extends StatelessWidget {
           (context) => ScreenUtilInit(
             designSize: const Size(375, 812),
             minTextAdapt: true,
-            enableScaleWH: () {
-              return true;
-            },
             splitScreenMode: true,
             child: MaterialApp(
               locale: Locale('en'),
               localizationsDelegates: customLocalizationDelegates,
               supportedLocales: S.delegate.supportedLocales,
-
               debugShowCheckedModeBanner: false,
               theme: lightTheme(),
               darkTheme: darkTheme(),
               themeMode: ThemeMode.system,
               onGenerateRoute: AppRouter.generateRoute,
-
-              home: homwiw(),
+              initialRoute: AppRoutes.onboardingView,
             ),
           ),
-    );
-  }
-}
-
-class homwiw extends StatelessWidget {
-  const homwiw({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    print(200.h);
-    print(100.w);
-
-    return Scaffold(
-      appBar: AppBar(
-        //  backgroundColor: Colors.red,
-        title: Text(
-          S.of(context).appName,
-          style: AppTextStyles.textStyleBold16,
-        ),
-      ),
-      body: Column(
-        mainAxisSize: MainAxisSize.min,
-        children: [
-          Container(height: 200.h, width: 200.w, color: Colors.orange),
-          Row(
-            children: [
-              Text(S.of(context).appName, style: AppTextStyles.textStyleBold16),
-            ],
-          ),
-          Text(S.of(context).appName, style: AppTextStyles.textStyleBold16),
-        ],
-      ),
     );
   }
 }
--- lib/config/routes/app_router.dart
+++ lib/config/routes/app_router.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
-import 'package:ninga/app.dart';
 import 'package:ninga/core/functions/transitions/page_route_builder_method.dart';
+import 'package:ninga/features/onboarding/presentation/view/onboarding_view.dart';
 
 class AppRouter {
   static Route? generateRoute(RouteSettings settings) {
@@ -8,7 +8,7 @@ class AppRouter {
       case AppRoutes.onboardingView:
         return pageRouteBuilderMethod(
           pageBuilder:
-              (context, animation, secondaryAnimation) => const Ninga(),
+              (context, animation, secondaryAnimation) => const OnboardingView(),
         );
 
       default:
