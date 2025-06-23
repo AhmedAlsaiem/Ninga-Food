b5b14a1630d540500f9d91565024a555cd7e0aeb
AhmedAlsaiem <ahmedalsaiem2025@gmail.com>
2025-06-23 16:45:22 +0300

    create onboarding view body and trigger it in onboarding view body

--- lib/features/onboarding/presentation/view/onboarding_view.dart
+++ lib/features/onboarding/presentation/view/onboarding_view.dart
@@ -1,10 +1,13 @@
 import 'package:flutter/material.dart';
+import 'package:ninga/features/onboarding/presentation/widget/onboarding_viewbody.dart';
 
 class OnboardingView extends StatelessWidget {
   const OnboardingView({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: OnboardingViewbody(),
+    );
   }
 }
---
+++ lib/features/onboarding/presentation/widget/onboarding_viewbody.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class OnboardingViewbody extends StatelessWidget {
+  const OnboardingViewbody({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Column();
+  }
+}
