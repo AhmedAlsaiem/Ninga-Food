7076a8d686a54ae64d3cb66ffe144f365f5e61be
AhmedAlsaiem <ahmedalsaiem2025@gmail.com>
2025-07-19 06:13:01 +0300

    rigister type addapter

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,14 @@
 import 'package:flutter/material.dart';
 import 'package:hive_flutter/adapters.dart';
 import 'package:ninga/app.dart';
+import 'package:ninga/core/data/app_state_model.dart';
 import 'package:ninga/core/utils/hive_keys.dart';
 
 void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
   await Hive.initFlutter();
   await Hive.openBox(HiveKeys.kBoxName);
+  Hive.registerAdapter(AppStateModelAdapter());
   runApp(Ninga());
 }
