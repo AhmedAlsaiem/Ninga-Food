4a4fa85b2f52f6831b33cc60e22350f61ec9605f
AhmedAlsaiem <ahmedalsaiem2025@gmail.com>
2025-07-19 05:13:32 +0300

    create hive key file this file contain all keys that used in hive

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:hive_flutter/adapters.dart';
 import 'package:ninga/app.dart';
 
-void main() {
+void main() async {
+  await Hive.initFlutter();
+  Hive.openBox('');
   runApp(Ninga());
 }
