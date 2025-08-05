a3168f68095d0aa1cfaae6fd4a1d29b2ad91f546
AhmedAlsaiem <ahmedalsaiem2025@gmail.com>
2025-08-05 17:00:09 +0300

    create change password ,login,signup methods in data source

--- lib/features/authentication/data/datasource/remote/auth_remote_datasource.dart
+++ lib/features/authentication/data/datasource/remote/auth_remote_datasource.dart
@@ -1,9 +1,10 @@
-import 'package:dartz/dartz.dart';
-import 'package:ninga/core/error/error_model.dart';
+import 'package:ninga/core/api/api_consumer.dart';
+import 'package:ninga/core/api/end_point.dart';
+import 'package:ninga/core/error/exception.dart';
+import 'package:ninga/core/sucess/sucess_model.dart';
 import 'package:ninga/features/authentication/data/model/user_auth_model.dart';
-import 'package:ninga/features/authentication/domain/entities/sucess_entity.dart';
 
-abstract class AuthRepo {
+abstract class BaseAuthRemoteDatasource {
   Future<UserAuthModel> signUp({
     required String email,
     required String phoneNumber,
@@ -14,7 +15,7 @@ abstract class AuthRepo {
     required String phoneNumber,
     required String password,
   });
-  Future<Either<ErrorModel, String>> changePassword({
+  Future<String> changePassword({
     required String password,
     required String newPassword,
   });
@@ -30,13 +31,128 @@ abstract class AuthRepo {
     required String email,
     required String confirmationCode,
   });
-  Future<Either<ErrorModel, SucessEntity>> sentCodeToVerifiyAcount({
-    required String email,
-  });
+  Future<SucessModel> sentCodeToVerifiyAcount({required String email});
   Future<UserAuthModel> getCurrentUser();
   Future<UserAuthModel> updateUser({
     required String userName,
     required String phoneNumber,
     required String email,
   });
+}
+
+class AuthRemoteDatasource extends BaseAuthRemoteDatasource {
+  ApiConsumer api;
+  AuthRemoteDatasource({required this.api});
+  @override
+  Future<String> changePassword({
+    required String password,
+    required String newPassword,
+  }) async {
+    try {
+      dynamic response = await api.post(
+        EndPoint.registerUser,
+        data: {ApiKey.newPassword: newPassword},
+      );
+      return response.toString();
+    } on ServerException catch (e) {
+      throw ServerException(errModel: e.errModel);
+    } on Exception catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<UserAuthModel> confirmeEmailByEmailCode({
+    required String email,
+    required String confirmationCode,
+  }) {
+    // TODO: implement confirmeEmailByEmailCode
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserAuthModel> getCurrentUser() {
+    // TODO: implement getCurrentUser
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserAuthModel> login({
+    required String phoneNumber,
+    required String password,
+  }) async {
+    try {
+      dynamic response = await api.post(
+        EndPoint.login,
+        data: {ApiKey.phoneNumber: phoneNumber, ApiKey.password: password},
+      );
+      UserAuthModel userData = UserAuthModel.fromJson(jsonData: response);
+      return userData;
+    } on ServerException catch (e) {
+      throw ServerException(errModel: e.errModel);
+    } on Exception catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<UserAuthModel> refreshToken({
+    required String token,
+    required String refreshToken,
+  }) {
+    // TODO: implement refreshToken
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserAuthModel> resetPassword({
+    required String email,
+    required String newPassword,
+  }) {
+    // TODO: implement resetPassword
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<SucessModel> sentCodeToVerifiyAcount({required String email}) {
+    // TODO: implement sentCodeToVerifiyAcount
+    throw UnimplementedError();
+  }
+
+  @override
+  Future<UserAuthModel> signUp({
+    required String email,
+    required String phoneNumber,
+    required String password,
+    required String name,
+  }) async {
+    try {
+      dynamic response = await api.post(
+        EndPoint.registerUser,
+        data: {
+          ApiKey.email: email,
+          ApiKey.phoneNumber: phoneNumber,
+          ApiKey.password: password,
+          ApiKey.fullName: name,
+          ApiKey.type: 1,
+        },
+      );
+      UserAuthModel userData = UserAuthModel.fromJson(jsonData: response);
+      return userData;
+    } on ServerException catch (e) {
+      throw ServerException(errModel: e.errModel);
+    } on Exception catch (e) {
+      throw Exception(e.toString());
+    }
+  }
+
+  @override
+  Future<UserAuthModel> updateUser({
+    required String userName,
+    required String phoneNumber,
+    required String email,
+  }) {
+    // TODO: implement updateUser
+    throw UnimplementedError();
+  }
 }
